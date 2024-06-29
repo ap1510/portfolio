ebe9ee4ef8d857662ba98b48947b030cf5ed591a
aman-uta-kgp <165545216+aman-uta-kgp@users.noreply.github.com>
2024-06-29 13:18:38 +0000

    pushing all files

---
+++ build/precache-manifest.6ff5d73123160cdda99eafc6eba0dd9e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5270c7373671c2c195d",
+    "url": "/portfolio/static/css/main.91d3fd10.chunk.css"
+  },
+  {
+    "revision": "a5270c7373671c2c195d",
+    "url": "/portfolio/static/js/main.79a30da7.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "633feee1c83340206ced",
+    "url": "/portfolio/static/css/2.2dab4b77.chunk.css"
+  },
+  {
+    "revision": "633feee1c83340206ced",
+    "url": "/portfolio/static/js/2.5add1241.chunk.js"
+  },
+  {
+    "revision": "6a4bbb989d0ee458467a788a6e883b4e",
+    "url": "/portfolio/static/media/image_cropped.6a4bbb98.png"
+  },
+  {
+    "revision": "35046de2df36d09042bc2c44839e8379",
+    "url": "/portfolio/static/media/BSM_model_image.35046de2.png"
+  },
+  {
+    "revision": "e73e4d2d0400d9b4c1fec37f55a918e4",
+    "url": "/portfolio/static/media/Binomial_CRR.e73e4d2d.png"
+  },
+  {
+    "revision": "bd287f65155a76c015ffc31b1be56fa6",
+    "url": "/portfolio/static/media/loan_amortization.bd287f65.jpg"
+  },
+  {
+    "revision": "f7d742fc14d3d495da2581043372d15a",
+    "url": "/portfolio/static/media/fastapi_sklearn.f7d742fc.png"
+  },
+  {
+    "revision": "23273bc8bc3197e29e433d7cf66185fc",
+    "url": "/portfolio/static/media/sklearn_genetic_opt.23273bc8.png"
+  },
+  {
+    "revision": "0928f3759e565a9b87ebd48e334e6028",
+    "url": "/portfolio/index.html"
+  }
+];
