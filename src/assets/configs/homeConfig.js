07ac6e5b4d425912ba821215eddb98f49e7803bf
Aditya Parashar <parasharaditya58@gmail.com>
2025-03-05 01:06:55 +0530

    Added personal details

--- src/assets/configs/homeConfig.js
+++ src/assets/configs/homeConfig.js
@@ -12,9 +12,9 @@ const homeConfig = {
 
     greeting:
         <div className="greeting-container">
-        <img src={profileImage} alt="Aman Sharma" className="profile-image" />
+        <img src={profileImage} alt="Aditya Parashar" className="profile-image" />
         <h1 className="heading">
-            Hi! I'm <strong className="main-name"> Aman Sharma</strong>
+            Hi! I'm <strong className="main-name"> Aditya Parashar</strong>
         </h1>
 </div>,
     titles: [
@@ -24,32 +24,39 @@ const homeConfig = {
         "An IITian"
     ],
     about: {
-        start: ["With ~3 years of experience in risk analytics coupled with an FRM certification,",
+        start: ["With ~3 years of experience in Data Science, ML modelling, Advanced analytics and Decision Science,",
         " I bring to the table expertise in the financial services industry and data science applications"
 
     ],
             
-        exit: "I am currently a grad student UT Austin's MS program in Business Analytics - Financial Analytics concentration."
+        exit: "I am currently a Data Science Manager at American Express, managing global card fraud risk."
     },
     workTimeline: [
         {
             id: "work-3",
-            title: ["Graduate Student : MS Business Analytics"],
+            title: ["Data Science Manager: American Express"],
             company: (  
                 <div>
                 <p style={{ fontSize: "larger", display: "block", marginTop: "0" }}>UT Austin</p>
                 <div style={{ fontSize: "smaller", display: "block", marginTop: "0" }}> McCombs School of Business</div>
             </div>
         ),
-            description : "Coursework : Advaned Machine Learning, Financial Modeling, Fixed Income Analysis",
-            date: "June' 24 - May' 25",
+        description: (<ul>
+            <li>Developed fundamental ML models (XGBoost, Neural Networks & RNN) improving fraud detection by 20%. </li>
+            <li>Leading a team of four, ensuring robust ML based fraud decisioning on 1B+ transactions monthly</li>
+            <li>Designed 25+ decision strategies, delivering $12M+ in annual savings.</li>
+        </ul>),
+           
+
+
+            date: "Aug'21 - Present",
             icon: <FaUserGraduate/>,
-            tags: ["python", "llm", "R", "SQL"]
+            tags: ["python", "SQL","ML Modelling","A/B Testing","Decision Analytics"]
         },
         
         {
             id: "work-2",
-            title: "Manager - Risk Analytics",
+            title: "Machine Learning Intern: Capital One",
             company: (  
                 <div>
                 <p style={{ fontSize: "larger", display: "block", marginTop: "0" }}>Drip Capital</p>
@@ -57,46 +64,46 @@ const homeConfig = {
             </div>
         ),
             description: (<ul>
-            <li>Managed portfolio risk of loan book sized 50 Mn+ USD, 150+ US SMEs </li>
-            <li>Developed ML based probabilistic credit and fraud risk controls</li>
-            <li>Onboarded new data vendors - credit bureaus, alternate data providers etc</li>
+            <li>Built NLP models (F1 score: 0.85) using SVC & TF-IDF to detect suboptimal dealership practices.</li>
+            <li>Identified 125+ fraudulent dealerships, mitigating brand and regulatory risks.</li>
+            <li>Developed automated risk-based decisioning for dealerships in California & Texas.</li>
         </ul>),
-            date: "Sep' 22 - May' 24",
+            date: "May' 20 - Jul' 20",
             icon: <BiRocket/>,
-            tags: ["ml", "AWS", "financial statement analysis", "credit risk modelling", "risk management", "PostgreSQL"]
+            tags: ["Python","Deep Learning","Unsupervised Learning","Customer Journey Analytics","Clustering & Dimensionality Reduction","Risk-Based Decisioning"]
         },
         {
             id: "work-3",
-            title: "Risk Analyst II",
+            title: "Machine Learning Intern: Colevit Technology",
             company: (  
                 <div>
                 <p style={{ fontSize: "larger", display: "block", marginTop: "0" }}>American Express</p>
                 <div style={{ fontSize: "smaller", display: "block", marginTop: "0" }}> US Consumer Cards Business</div>
             </div>),
             description: (<ul>
-                <li>Managed Sentinel, pre - underwriting risk control, baking in company's macro economic outlook into model threholds</li>
-                <li>Nominated for Howard's Advisory Board as B30 & 35 representative </li>
+                <li>IoT Security: Developed a smart city security prototype on a distributed IoT infrastructure</li>
+                <li>Vehicle Detection: Achieved 0.72 precision in real-time monitoring using MobileNet SSD</li>
+                <li>Facial Recognition: Enhanced security efficiency 5x with YOLOv3-integrated smart cameras</li>
             </ul>),
-            date: "Aug' 21 - Sep' 22",
+            date: "May' 19 - Jul' 19",
             icon: <BsBank2/>,
-            tags: ["boosting", "HiveQL", "python", "SAS", "leadership"]
+            tags: ["IoT Security","Deep Learning"]
         },
         {
             id: "work-2",
-            title: "Undergraduate & Graduate Student",
+            title: "Undergraduate Student",
             company: (  
                 <div>
                 <p style={{ fontSize: "larger", display: "block", marginTop: "0" }}>IIT Kharagpur (KGP)</p>
                 <div style={{ fontSize: "smaller", display: "block", marginTop: "0" }}> Bachelors & Masters in Chemical Engineering</div>
             </div>),
             description: (<ul>
-                <li>Awared Honorable Mention at graduation</li>
-                <li>International Internship - ML Research Intern, NTHU Taiwan </li>
-                <li>Student Activities - Governor, Business Club; Captain, Inter IIT Case Study Team, NSS Volunteer </li>
+                <li>Relevant Coursework: ML in medicine,AI: Foundations & Applications, Expert Systems, Data Structures and Algorithms</li>
+                <li>Student Activities - Governor, Business Club; Vice Captain, Inter IIT Case Study Team, NSS Volunteer </li>
             </ul>),
-            date: "Jul' 16 - Jul' 21",
+            date: "Jul' 17 - Jul' 21",
             icon: <IoSchoolSharp/>,
-            tags: ["C", "C++", "MATLAB", "neural networks","bayesian optimization", "volunteering"]
+            tags: ["C", "C++","Python","MATLAB", "neural networks","bayesian optimization", "volunteering"]
         }
     ]
 }
