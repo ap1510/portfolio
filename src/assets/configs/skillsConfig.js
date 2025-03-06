36be2c059dc79d3bee00b57fdecfc91acfc00096
Aditya Parashar <parasharaditya58@gmail.com>
2025-03-07 01:45:47 +0530

    Changed directory name

--- src/assets/configs/skillsConfig.js
+++ src/assets/configs/skillsConfig.js
@@ -2,8 +2,7 @@ import {
     SiPython,
     SiPostgresql,
     SiRstudio,
-    SiMetabase,
-    SiPowerbi
+    SiGooglecloud
     
 } from "react-icons/si";
 import {GrMysql} from "react-icons/gr";
@@ -25,46 +24,46 @@ const skillsConfig = {
         {
             id: "skills-1",
             className: "skill-icon",
-            icon: <SiRstudio size={50}/>,
-            text: "R"
+            icon: <SiPostgresql size={50}/>,
+            text: "PostgreSQL"
         },
         {
             id: "skills-2",
             className: "skill-icon",
-            icon: <TbCircleLetterC size={50}/>,
-            text: "C / C++"
+            icon: <GrMysql size={50}/>,
+            text: "MySQL"
         },
         {
             id: "skills-3",
+            className: "skill-icon",
+            icon: <TbCircleLetterC size={50}/>,
+            text: "C/C++"
+        },
+        {
+            id: "skills-4",
             className: "skill-icon",
             icon: <TbMathFunction size={50}/>,
             text: "MATLAB"
         },
         {
-            id: "skills-4",
-            className: "skill-icon",
-            icon: <SiPostgresql size={50}/>,
-            text: "PostgreSQL"
-        },
-        {
             id: "skills-5",
             className: "skill-icon",
-            icon: <GrMysql size={50}/>,
-            text: "MySQL"
+            icon: <SiRstudio size={50}/>,
+            text: "R"
         }
     ],
     complementarySkills: [
         {
             id: "skills-6",
             className: "skill-icon",
-            icon: <FaAws size={50}/>,
-            text: "AMS Sagemaker"
+            icon: <SiGooglecloud size={50}/>,
+            text: "Google Cloud Platform"
         },
         {
             id: "skills-7",
             className: "skill-icon",
-            icon: <SiPowerbi size={50}/>,
-            text: "Power BI"
+            icon: <FaAws size={50}/>,
+            text: "AWS (S3,Lambda,Sagemaker)"
         },
         {
             id: "skills-8",
@@ -81,8 +80,8 @@ const skillsConfig = {
         {
             id: "skills-10",
             className: "skill-icon",
-            icon: <SiMetabase size={50}/>,
-            text: "Metabase"
+            icon: <FaGitAlt size={50}/>,
+            text: "Git"
         }
         
         
